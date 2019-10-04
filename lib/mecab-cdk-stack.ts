6890c5168f8c99bff88362317cdf902dbd54125a
HAHURI <tokitsukaze.com@gmail.com>
2019-10-04 20:51:41 +0900

    にゃんで？

--- lib/mecab-cdk-stack.ts
+++ lib/mecab-cdk-stack.ts
@@ -6,8 +6,12 @@ export class MecabCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaLayer = new lambda.LayerVersion(this,'LambdaLayer',{
-      code: lambda.Code.asset('layer'),
+    const neologdLayer = new lambda.LayerVersion(this,'NeologdLayer',{
+      code: lambda.Code.asset('neologdLayer'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_10_X]
+    })
+    const ipadicLayer = new lambda.LayerVersion(this,'IpadicLayer',{
+      code: lambda.Code.asset('ipadicLayer'),
       compatibleRuntimes: [lambda.Runtime.NODEJS_10_X]
     })
     
@@ -19,7 +23,7 @@ export class MecabCdkStack extends cdk.Stack {
         environment: {
             REGION: 'ap-northeast-1'
         },
-        layers: [lambdaLayer],
+        layers: [neologdLayer,ipadicLayer],
         memorySize: 256
     });
     const MecabLambdaVersion = new lambda.Version(this,'MecabLambdaVersion',{
