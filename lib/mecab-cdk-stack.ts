c7ab912fa623a0a751bec86de60142e565175e32
HAHURI <tokitsukaze.com@gmail.com>
2019-10-05 19:12:15 +0900

    api-gateway

--- lib/mecab-cdk-stack.ts
+++ lib/mecab-cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as lambda  from '@aws-cdk/aws-lambda';
 import cdk = require('@aws-cdk/core');
 import { Duration } from '@aws-cdk/core';
+import { RestApi, Integration, LambdaIntegration, Resource,
+  MockIntegration, PassthroughBehavior, EmptyModel } from "@aws-cdk/aws-apigateway"
+
 
 export class MecabCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -31,6 +34,48 @@ export class MecabCdkStack extends cdk.Stack {
       lambda: MecabLambda,
       description: 'メモ'
     })
+
+    // API Gateway 作成
+    const restApi: RestApi = new RestApi(this, "MecabAPI", {
+      restApiName: "MecabAPI", // API名
+      description: "Deployed by CDK" // 説明
+    })
+    // Integration 作成
+    const integration: Integration = new LambdaIntegration(MecabLambda)
+    // リソースの作成
+    const getResouse: Resource = restApi.root.addResource("get")
+    // メソッドの作成
+    getResouse.addMethod("GET", integration)
+    // CORS対策でOPTIONSメソッドを作成
+    getResouse.addMethod("OPTIONS", new MockIntegration({
+      integrationResponses: [{
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Access-Control-Allow-Headers":
+            "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+          "method.response.header.Access-Control-Allow-Origin": "'*'",
+          "method.response.header.Access-Control-Allow-Credentials": "'false'",
+          "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET,PUT,POST,DELETE'",
+        }
+      }],
+      passthroughBehavior: PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": "{\"statusCode\": 200}"
+      }
+    }), {
+      methodResponses: [{
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Access-Control-Allow-Headers": true,
+          "method.response.header.Access-Control-Allow-Origin": true,
+          "method.response.header.Access-Control-Allow-Credentials": true,
+          "method.response.header.Access-Control-Allow-Methods": true,
+        },
+        responseModels: {
+          "application/json": new EmptyModel()
+        },
+      }]
+    });
   }
 }
 
