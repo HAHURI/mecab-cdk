d98db7c013bd14e53a72bee77dc297b6e5411754
HAHURI <tokitsukaze.com@gmail.com>
2019-10-08 22:35:05 +0900

    とりあえず

--- src/vcasAnalyzeLambda/vcas-analyse-cdk.ts
+++ src/vcasAnalyzeLambda/vcas-analyse-cdk.ts
@@ -1,5 +1,5 @@
 declare function require(x: string): any;
-const neo4j = require('neo4j-driver')
+const neo4j = require('neo4j-driver').v1
 const requestPromise = require('request-promise');
 const parser = require('xml2json');
 const driver = neo4j.driver(process.env.NEO4J_URL,neo4j.auth.basic(process.env.USER_NAME, process.env.USER_PASSWORD))
@@ -26,26 +26,27 @@ export class VcasAnalyzeLambda {
     public static async hello(event: any): Promise<any> {
         // 想定するリクエスト
         var session = driver.session();        
-        let liveList = getApiResponse(createApiOptions('GET','https://api.virtualcast.jp/channels/ja/archive/list',60,undefined,{count:'20',offsetId:event.queryStringParameters.offsetId}))
-        let res = liveList.list.forEach(async function(live:any){
-            // neo4jに登録があるかを確認する
+        let liveList = await getApiResponse(createApiOptions('GET', 'https://api.virtualcast.jp/channels/ja/archive/list?count=20&offsetId='+event.queryStringParameters.offsetId, 60));
+        for(let i =0; i<liveList.list.length; i++){
+            console.log(liveList.list[i])
             var readTxResultPromise = await session.readTransaction(function (transaction:any) {
-                var result = transaction.run('MATCH (live:Live { liveId:"' + live.id + '"}) RETURN live');
+                var result = transaction.run('MATCH (live:Live { liveId:"' + liveList.list[i].id + '"}) RETURN live');
                 return result;
             });
-            if(readTxResultPromise.records.length===0){
-                if(live.url.match(/nicovideo/)){
-                    let niconama = getResponse(createApiOptions('GET','http://namagome.com/come_dl.cgi/'+live.url.split('/')[-1]+'/xml',60))
-                    console.log(niconama)
-                    console.log(parser.toJson(niconama))
-                    return ''
-                } else {
-                    return ''
-                }    
-            }else{
-                return 'すでに登録済みです'
+            console.log(readTxResultPromise.records.length)
+            if (readTxResultPromise.records.length === 0) {
+                if (liveList.list[i].url.match(/nicovideo/)) {
+                    let niconama = getResponse(createApiOptions('GET', 'http://namagome.com/come_dl.cgi/' + liveList.list[i].url.split('/')[-1] + '/xml', 60));
+                    console.log(niconama);
+                    console.log(parser.toJson(niconama));
+                    return '';
+                }else {
+                    console.log('ニコ生以外です')
+                }
+            } else {
+                console.log('すでに登録済みです')
             }
-        })
+        }
         // 生米保管庫からXMLを取得する
         // mecabでテキストデータを解析する
         // neo4jに解析結果を保存する
