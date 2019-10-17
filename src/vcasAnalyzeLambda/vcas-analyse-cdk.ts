5f92998f8d60cf68ddf173e35ceceb2cae110f50
HAHURI <tokitsukaze.com@gmail.com>
2019-10-17 22:57:34 +0900

    配信Edgeまでは実装

--- src/vcasAnalyzeLambda/vcas-analyse-cdk.ts
+++ src/vcasAnalyzeLambda/vcas-analyse-cdk.ts
@@ -43,70 +43,204 @@ export class VcasAnalyzeLambda {
         // 想定するリクエスト
         var session = driver.session();        
         let liveList:{list:Live[]} = await getApiResponse(createApiOptions('GET', 'https://api.virtualcast.jp/channels/ja/archive/list?count=20&offsetId='+event.queryStringParameters.offsetId, 6000));
+        /**
+         * <User> --Broadcast--→ <Live> ←--CommentCount-- <User>
+         * <User> --Count--→ <Word> --Count--→ <Live> ←--Broadcast-- <User>
+         * <Comment> --typology-- <Comment> 
+         * <Word> --typology-- <Word> 
+         */
         for(let i =0; i<liveList.list.length; i++){
-            console.log(liveList.list[i])
+            let livedata = liveList.list[i]
+            console.log(livedata)
+            
+            // Live
             var readTxResultPromise = await session.readTransaction(function (transaction:any) {
-                var result = transaction.run('MATCH (live:Live { liveId:"' + liveList.list[i].id + '"}) RETURN live');
+                var result = transaction.run('MATCH (live:Live { id:' + livedata.id + '}) RETURN live');
                 return result;
             });
-            /**
-             * <User> --Broadcast--→ <Live> ←--CommentCount-- <User>
-             * <User> --Count--→ <Word> --Count--→ <Live> ←--Broadcast-- <User>
-             * <Comment> --typology-- <Comment> 
-             * <Word> --typology-- <Word> 
-             */
-            console.log(readTxResultPromise.records.length)
             if (readTxResultPromise.records.length === 0) {
-                /** live
-                 * id,title,description,thumbnail,url,platform,viewers,uniqueViewers,comments,nickname,studioMetadata,startedAt,endedAt,createdAt
-                */
-                let livedata = liveList.list[i]
-                session.run('CREATE (live:Live { id: '+livedata.id
-                    +',title: '+livedata.title
-                    +',description: '+livedata.description
-                    +',thumbnail: '+livedata.thumbnail
-                    +',url: '+livedata.url
-                    +',platform: '+livedata.platform
-                    +',nickname: '+livedata.nickname
-                    +',studioMetadata: '+livedata.studioMetadata
-                    +',viewers: {viewersParam},uniqueViewers: {uniqueViewersParam},comments: {commentsParam},startedAt: {startedAtParam},endedAt: {endedAtParam},createdAt: {createdAtParam}})',
+                await session.run('CREATE (live:Live { id: '+livedata.id
+                    +',title: "'+livedata.title.replace(/"/g,'&quot;')
+                    +'",description: "'+livedata.description.replace(/"/g,'&quot;')
+                    +'",thumbnail: "'+livedata.thumbnail.replace(/"/g,'&quot;')
+                    +'",url: "'+livedata.url.replace(/"/g,'&quot;')
+                    +'",platform: "'+livedata.platform
+                    +'",nickname: "'+livedata.nickname.replace(/"/g,'&quot;')
+                    +'",studioMetadata: "'+livedata.studioMetadata.toString
+                    +'",viewers: {viewersParam},uniqueViewers: {uniqueViewersParam},comments: {commentsParam},startedAt: {startedAtParam},endedAt: {endedAtParam},createdAt: {createdAtParam}})',
                     { 
-                        viewersParam: neo4j.int(livedata.viewers),
-                        uniqueViewersParam: neo4j.int(livedata.uniqueViewers),
-                        commentsParam: neo4j.int(livedata.comments),
-                        startedAtParam: neo4j.date(livedata.startedAt*1000),
-                        endedAtParam: neo4j.date(livedata.endedAt*1000),
-                        createdAtParam: neo4j.date(livedata.createdAt*1000)
+                        viewersParam: neo4j.int(livedata.viewers!==null? livedata.viewers: -1),
+                        uniqueViewersParam: neo4j.int(livedata.uniqueViewers!==null? livedata.uniqueViewers: -1),
+                        commentsParam: neo4j.int(livedata.comments!==null? livedata.comments: -1),
+                        startedAtParam: neo4j.int(livedata.startedAt*1000),
+                        endedAtParam: neo4j.int(livedata.endedAt*1000),
+                        createdAtParam: neo4j.int(livedata.createdAt*1000)
                     }
                 )
-                /*if (liveList.list[i].url.match(/nicovideo/)) {
-                    /** user,lived,comment,word
-                     * NiconamaComment.PlayerStatus.Stream
-                     *      OwnerId,OwnerName,DefaultCommunity
-                     * NiconamaComment.LiveCommentDataArray.chat[]
-                     *      anonymity,date,mail,no,thread,user_id,vpos,locale,$t
-                    *
-                    let niconama = await getResponse(createApiOptions('GET', 'http://namagome.com/come_dl.cgi/' + liveList.list[i].url.split('/')[4].replace(/[^0-9]/g, '') + '/xml', 6000));
-                    let niconamaJson = JSON.parse(parser.toJson(niconama))
-                    if (typeof niconamaJson.NiconamaComment.LiveInfo.LiveTitle === 'string'){
-                        console.log(niconamaJson)
-                    }else{
-                        console.log('コメデータはなかったのです')
-                    }
-                }else {
-                    console.log('ニコ生以外です')
-                }*/
-            } else {
-                console.log('すでに登録済みです')
             }
+
+            // User
+            const result = when(livedata.platform)
+                .on(v => v === 'nicolive', () => nicoliveFnc(livedata))
+                .on(v => v === 'youtubelive', () => youtubeliveFnc(livedata))
+                .on(v => v === 'twitcasting', () => twitcastingFnc(livedata))
+                .on(v => v === 'twitch', () => twitchFnc(livedata))
+                .on(v => v === 'showroom', () => otherFnc(livedata))
+                .otherwise(() => otherFnc(livedata))
+            console.log(result)
         }
-        // 生米保管庫からXMLを取得する
-        // mecabでテキストデータを解析する
-        // neo4jに解析結果を保存する
     }    
 }
 
-
+// youtubelive: URLからチャンネルの判定が可能：ニックネームとチャンネルID
+export async function youtubeliveFnc(livedata:Live):Promise<string>{
+    var session = driver.session()
+    const channelId = livedata.url.split('/')[4].split('?')[0]
+    // node(User)
+    var Neo4jUser = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (user:User { channelId: "'+ channelId +'"}) RETURN user');
+        return result;
+    });
+    if (Neo4jUser.records.length === 0) {
+        await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", channelId: "'+ channelId +'"})')
+    }
+    // edge(User->Live)
+    var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (:User { channelId: "'+ channelId +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+        return result;
+    });
+    if (Neo4jUserLiveEdge.records.length === 0) {
+        await session.run('MATCH (user:User { channelId: "'+ channelId +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+    }
+    console.log('youtubeliveの処理が完了しました！')
+    return '処理が完了しました！'
+}
+// twitcasting: URLからユーザの判定が可能：ニックネームとtwitcastingのID
+export async function twitcastingFnc(livedata:Live):Promise<string>{
+    var session = driver.session()
+    const twitcastingId = livedata.url.split('/')[3]
+    // node(User)
+    var Neo4jUser = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (user:User { twitcastingId: "'+ twitcastingId +'"}) RETURN user');
+        return result;
+    });
+    if (Neo4jUser.records.length === 0) {
+        await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", twitcastingId: "'+ twitcastingId +'"})')
+    }
+    // edge(User->Live)
+    var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (:User { twitcastingId: "'+ twitcastingId +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+        return result;
+    });
+    if (Neo4jUserLiveEdge.records.length === 0) {
+        await session.run('MATCH (user:User { twitcastingId: "'+ twitcastingId +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+    }
+    console.log('twitcastingの処理が完了しました！')
+    return '処理が完了しました！'
+}
+// twitch: URLからユーザの判定が可能：ニックネームとtwitchのID
+export async function twitchFnc(livedata:Live):Promise<string>{
+    var session = driver.session()
+    const twitchId = livedata.url.split('/')[3]
+    // node(User)
+    var Neo4jUser = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (user:User { twitchId: "'+ twitchId +'"}) RETURN user');
+        return result;
+    });
+    if (Neo4jUser.records.length === 0) {
+        await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", twitchId: "'+ twitchId +'"})')
+    }
+    // edge(User->Live)
+    var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (:User { twitchId: "'+ twitchId +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+        return result;
+    });
+    if (Neo4jUserLiveEdge.records.length === 0) {
+        await session.run('MATCH (user:User { twitchId: "'+ twitchId +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+    }
+    console.log('twitchの処理が完了しました！')
+    return '処理が完了しました！'
+}
+// showroom: URLからユーザの判定はできない：ニックネームのみ
+export async function otherFnc(livedata:Live):Promise<string>{
+    var session = driver.session()
+    // node(User)
+    var Neo4jUser = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (user:User { liveId: '+ livedata.id +'}) RETURN user');
+        return result;
+    });
+    if (Neo4jUser.records.length === 0) {
+        await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", liveId: "'+ livedata.id +'"})')
+    }
+    // edge(User->Live)
+    var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+        var result = transaction.run('MATCH (:User { liveId: "'+ livedata.id +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+        return result;
+    });
+    if (Neo4jUserLiveEdge.records.length === 0) {
+        await session.run('MATCH (user:User { liveId: "'+ livedata.id +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+    }
+    console.log('showroomの処理が完了しました！')
+    return '処理が完了しました！'
+}
+// nicolive: URLからユーザ判定できない：次の取得ができなかったらニックネームのみ、できたらニックネームと情報
+export async function nicoliveFnc(livedata:Live):Promise<string>{
+    var session = driver.session()
+    
+    // 生米保管庫からXMLを取得する
+    // mecabでテキストデータを解析する
+    // neo4jに解析結果を保存する
+    /** user,lived,comment,word
+     * NiconamaComment.PlayerStatus.Stream
+     *      OwnerId,OwnerName,DefaultCommunity
+     * NiconamaComment.LiveCommentDataArray.chat[]
+     *      anonymity,date,mail,no,thread,user_id,vpos,locale,$t
+     */
+    let niconama = await getResponse(createApiOptions('GET', 'http://namagome.com/come_dl.cgi/' + livedata.url.split('/')[4].replace(/[^0-9]/g, '') + '/xml', 6000));
+    let niconamaJson = JSON.parse(parser.toJson(niconama))
+    if (typeof niconamaJson.NiconamaComment.LiveInfo.LiveTitle === 'string'){
+        console.log(niconamaJson)
+        const ownerId = niconamaJson.NiconamaComment.PlayerStatus.Stream.OwnerId
+        // node(User)
+        var Neo4jUser = await session.readTransaction(function (transaction:any) {
+            var result = transaction.run('MATCH (user:User { ownerId: "'+ ownerId +'"}) RETURN user');
+            return result;
+        });
+        if (Neo4jUser.records.length === 0) {
+            await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", ownerId: "'+ ownerId +'", OwnerName: "'+ niconamaJson.NiconamaComment.PlayerStatus.Stream.OwnerName.replace(/"/g,'&quot;') +'"})')
+        }
+        // edge(User->Live)
+        var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+            var result = transaction.run('MATCH (:User { ownerId: "'+ ownerId +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+            return result;
+        });
+        if (Neo4jUserLiveEdge.records.length === 0) {
+            await session.run('MATCH (user:User { ownerId: "'+ ownerId +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+        }
+        // TODO: コメントデータ解析
+        console.log('nicolive(コメントあり)の処理が完了しました！')
+        return '処理が完了しました！'
+    }else{
+        // node(User)
+        var Neo4jUser = await session.readTransaction(function (transaction:any) {
+            var result = transaction.run('MATCH (user:User { liveId: '+ livedata.id +'}) RETURN user');
+            return result;
+        });
+        if (Neo4jUser.records.length === 0) {
+            await session.run('CREATE (user:User {nickname: "'+livedata.nickname.replace(/"/g,'&quot;') + '", liveId: "'+ livedata.id +'"})')
+        }
+        // edge(User->Live)
+        var Neo4jUserLiveEdge = await session.readTransaction(function (transaction:any) {
+            var result = transaction.run('MATCH (:User { liveId: "'+ livedata.id +'"})-[r:Broadcast]->(:Live { id:' + livedata.id + '}) RETURN r');
+            return result;
+        });
+        if (Neo4jUserLiveEdge.records.length === 0) {
+            await session.run('MATCH (user:User { liveId: "'+ livedata.id +'"}) , (live:Live { id:' + livedata.id + '}) CREATE (user)-[:Broadcast]->(live)')
+        }
+        console.log('nicolive(コメントなし)の処理が完了しました！')
+        return '処理が完了しました！'
+    }
+}
 
 
 // 共通関数
@@ -153,3 +287,24 @@ export function getApiResponse(options:HttpOptions): any{
     })
 }
 
+
+type ChainedWhen<T, R> = {
+    on: <A>(pred: (v: T) => boolean, fn: () => A) => ChainedWhen<T, R | A>;
+    otherwise: <A>(fn: () => A) => R | A;
+};
+
+const match = <T, R>(val: any): ChainedWhen<T, R> => ({
+    on: <A>(pred: (v: T) => boolean, fn: () => A) => match<T, R | A>(val),
+    otherwise: <A>(fn: () => A): A | R => val
+});
+
+const chain = <T, R>(val: T): ChainedWhen<T, R> => ({
+    on: <A>(pred: (v: T) => boolean, fn: () => A) =>
+    pred(val) ? match(fn()) : chain<T, A | R>(val),
+    otherwise: <A>(fn: () => A) => fn()
+});
+
+const when = <T>(val: T) => ({
+    on: <A>(pred: (v: T) => boolean, fn: () => A) =>
+    pred(val) ? match<T, A>(fn()) : chain<T, A>(val)
+});
